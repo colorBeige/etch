6845ad42579e2153f3954a4b0f1be88ac6b3fef7
colorBeige <133912252+colorBeige@users.noreply.github.com>
2024-08-07 05:40:49 -0400

    Implement grid size resizing

--- etch.js
+++ etch.js
@@ -2,20 +2,35 @@ const grid = document.querySelector(".grid");
 const sliderValue = document.querySelector(".slider");
 
 document.addEventListener("DOMContentLoaded", () => {
-    initializeGrid();
+    createGrid();
 });
 
-function initializeGrid() {
+sliderValue.addEventListener("change", () => {
+    resizeGrid();
+});
+
+function createGrid() {
     const cellCount = sliderValue.value;
     const gridStyles = window.getComputedStyle(grid);
     const gridSize = parseInt(gridStyles.width);
+    const cellSize = gridSize / cellCount;
 
     for (let i = 0; i < cellCount * cellCount; i++) {
         const gridCell = document.createElement("div");
-        gridCell.style.width = `${(gridSize / cellCount)}px`;
-        gridCell.style.height = `${(gridSize / cellCount)}px`;
+        gridCell.style.width = `${cellSize}px`;
+        gridCell.style.height = `${cellSize}px`;
         gridCell.classList.add("grid-cell");
         grid.appendChild(gridCell);
         
     }
+}
+
+function resizeGrid() {
+    //Removes all previous cells
+    const prevCellCount = grid.querySelectorAll(".grid-cell");
+    prevCellCount.forEach(cell => {
+        grid.removeChild(cell);
+    })
+
+    createGrid();
 }
